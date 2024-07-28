2b6a2242e864a3efce99d5e736e618d241e72df8
Seohyun Ryu <79790536+ruruisryu@users.noreply.github.com>
2024-07-28 17:20:50 +0900

    Title: 최소 경로로 탈출 하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240728/최소 경로로 탈출 하기/escape-with-min-distance.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef pair<int, int> pos;
+#define X first
+#define Y second
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
+
+bool board[100][100];
+int dist[100][100];
+
+int main() {
+    // 입력
+    int n, m;
+    cin >> n >> m;
+
+    for(int i=0 ;i<n; i++){
+        for(int j=0; j<n; j++){
+            cin >> board[i][j];
+        }
+    }
+
+    queue<pos> Q;
+    dist[0][0] = 1;
+    Q.push({0,0});
+
+    while(!Q.empty()){
+        pos cur = Q.front(); Q.pop();
+        for(int i=0; i<4; i++){
+            int nx = cur.X + dx[i];
+            int ny = cur.Y + dy[i];
+            if(nx<0 || nx>n || ny<0 || ny>n) continue;
+            if(dist[nx][ny] != 0 || board[nx][ny] == 0)continue;
+            dist[nx][ny] = dist[cur.X][cur.Y] +1;
+            Q.push({nx, ny});
+        }
+    }
+
+
+    cout << dist[n-1][n-1]-1;
+}
