21597072cd89e94923dc39b30cdf71681ac9907d
Seohyun Ryu <79790536+ruruisryu@users.noreply.github.com>
2024-08-18 23:27:46 +0900

    Title: 삼각형 컨베이어 벨트, Time: 169ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240818/삼각형 컨베이어 벨트/conveyor-belt-triangle.py
@@ -0,0 +1,31 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+n, time = map(int, input().split(" "))
+
+graph = []
+
+for _ in range(3):
+    temp = list(map(int, input().rstrip().split(" ")))
+    graph.append(temp)
+
+
+
+for t in range(time):
+
+    temp = deque([x[-1] for x in graph])
+    temp.appendleft(temp.pop())
+
+    for i in range(3):
+        for j in range(n-1, 0, -1):
+            graph[i][j] = graph[i][j-1]
+        
+
+    for i in range(3):
+        graph[i][0] = temp[i]
+    
+    
+for row in graph:
+    print(*row)
