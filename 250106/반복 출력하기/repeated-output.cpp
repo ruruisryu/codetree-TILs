17b141326d24df711fa23e237a9fc7101c69e2cc
Seohyun Ryu <79790536+ruruisryu@users.noreply.github.com>
2025-01-06 00:31:41 +0900

    Title: 반복 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250106/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+int main() {
+    cin >> N;
+
+    // Write your code here!
+    for( int i=0; i<N; i++){
+        cout << "12345^&*()_\n";
+    }
+
+    return 0;
+}
